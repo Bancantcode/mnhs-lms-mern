8ca5d5d428cbd20956391e07139f27fa6646a597
welru <reuelchristian.sundiam04@gmail.com>
2024-12-11 13:27:22 +0800

    try for hosting

--- server/config/db_config.js
+++ server/config/db_config.js
@@ -4,19 +4,31 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const db = new pg.Pool({
-    user: process.env.DB_USER || "postgres",
-    host: process.env.DB_HOST || "localhost",
-    database: process.env.DB_NAME || "MNHS-DB",
-    password: process.env.DB_PASS || "admin",
-    port: process.env.DB_PORT || 5432
-});
+// const db = new pg.Pool({
+//     user: process.env.DB_USER || "postgres",
+//     host: process.env.DB_HOST || "localhost",
+//     database: process.env.DB_NAME || "MNHS-DB",
+//     password: process.env.DB_PASS || "admin",
+//     port: process.env.DB_PORT || 5432
+// });
 
-const sequelize = new Sequelize("MNHS-DB", "postgres", "admin", { // change to process.env
-  host: 'localhost',
+// const sequelize = new Sequelize("MNHS-DB", "postgres", "admin", { // change to process.env
+//   host: 'localhost',
+//   dialect: 'postgres',
+// });
+
+const sequelize = new Sequelize({
   dialect: 'postgres',
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  ssl: true,
+  clientMinMessages: 'notice',
 });
 
-db.connect();
+// db.connect();
 
-export { db, sequelize };
+// export { db, sequelize };
+export { sequelize };
